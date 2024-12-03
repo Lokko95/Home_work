c3723f042583ac3cbf6bd5629039fdefd87be62a
lokko <jamm7022@gmail.com>
2024-12-03 22:20:21 +0300

    ffff

--- ggg.js
+++ ggg.js
@@ -5,12 +5,11 @@ spusocItems.forEach(item => {
         const submenu = this.querySelector('.submenu');
         if (submenu) {
             submenu.style.display = submenu.style.display === 'block' ? 'none' : 'block';
-            event.stopPropagation(); // Предотвращаем всплытие события
+            event.stopPropagation();
         }
     });
 });
 
-// Закрытие подменю при клике вне его
 document.addEventListener('click', function() {
     const submenus = document.querySelectorAll('.submenu');
     submenus.forEach(submenu => {
@@ -19,60 +18,47 @@ document.addEventListener('click', function() {
 });
 
 
-
-
-
-
-
-// Получаем элементы модального окна и кнопок
 const modal = document.getElementById('modal');
 const playstoreButton = document.getElementById('playstore-button');
 const appstoreButton = document.getElementById('appstore-button');
 const confirmButton = document.getElementById('confirm-button');
 const closeButton = document.getElementsByClassName('close')[0];
 
-let currentPlatform = ''; // Хранит текущую нажимаемую платформу
+let currentPlatform = ''; 
 
-// Обработчик событий для кнопок
+
 playstoreButton.addEventListener('click', function() {
     currentPlatform = 'Playstore';
-    modal.style.display = 'block'; // Открываем модальное окно
+    modal.style.display = 'block'; 
 });
 
 appstoreButton.addEventListener('click', function() {
     currentPlatform = 'App Store';
-    modal.style.display = 'block'; // Открываем модальное окно
+    modal.style.display = 'block'; 
 });
 
-// Обработчик для подтверждения перехода
 confirmButton.addEventListener('click', function() {
     window.open(currentPlatform === 'Playstore' ? 'https://play.google.com/store' : 'https://www.apple.com/app-store/', '_blank');
-    modal.style.display = 'none'; // Закрываем модальное окно
+    modal.style.display = 'none'; 
 });
 
-// Закрываем модальное окно при нажатии на "X"
 closeButton.addEventListener('click', function() {
     modal.style.display = 'none';
 });
 
-// Закрываем модальное окно при клике вне его
 window.addEventListener('click', function(event) {
     if (event.target === modal) {
         modal.style.display = 'none';
     }
 });
 
-
-
-
-// Получаем все элементы с классом icon-item
 const icons = document.querySelectorAll('.icon-item');
 
-// Добавляем обработчик события для каждого элемента
+
 icons.forEach(icon => {
     icon.addEventListener('click', function() {
         this.classList.add('bounce');
-        // Удаляем класс через 0.5 секунды
+
         setTimeout(() => {
           this.classList.remove('bounce');
         }, 500);
